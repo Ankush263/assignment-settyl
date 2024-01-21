a58e5c5c973e34eeccab1b60782cbc39ed5bea9d
Ankush263 <ankushbanik263@gmail.com>
2024-01-22 00:15:13 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorControllers');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const path = require('path');
+const io = require('socket.io')(5000, {
+	cors: {
+		origin: 'http://127.0.0.1:5173',
+		methods: ['GET', 'POST'],
+	},
+});
+
+const app = express();
+
+app.enable('trust proxy');
+
+app.use(
+	cors({
+		origin: '*',
+		credentials: true,
+	})
+);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+app.use(express.json());
+
+app.use(helmet());
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
+app.use(cookieParser());
+app.use(express.json({ limit: '10kb' }));
+
+app.use(mongoSanitize());
+app.use(xss());
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+io.on('connection', (socket) => {
+	console.log('connected');
+
+	socket.on('send-changes', (delta) => {
+		console.log(delta);
+		socket.broadcast.emit('receive-changes', delta);
+	});
+});
+
+// app.use('/api/v1/allUser', allUserRouter);
+
+app.all('*', (req, res, next) => {
+	next(new AppError(`Can't find ${req.originalUrl} on this server!!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
